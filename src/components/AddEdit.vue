95f182bab881812b013342ca7f7d5167eaf76c15
achtarudin <cabe2cabean214@gmail.com>
2018-04-01 16:02:29 +0700

    add component AddEdit.vue

--- src/components/AddEdit.vue
+++ src/components/AddEdit.vue
@@ -3,31 +3,55 @@
     <div class="card-body border border-primary">
       <div class="row">
         <div class="col">
-          <input type="text" class="form-control" placeholder="First name">
+          <input type="text" class="form-control" placeholder="Name" v-model="name">
         </div>
         <div class="col">
-          <input type="text" class="form-control" placeholder="Last name">
+          <input type="text" class="form-control"
+          placeholder="Last name" v-model="numberPhone">
         </div>
       </div>
       <br>
-      <ul class="nav justify-content-end">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Save</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " href="#">Reset</a>
-        </li>
-      </ul>
+      <div class="row">
+        <div class="col-md-8 offset-md-10 col-sm-4 offset-sm-9">
+          <button type="submit"
+          class="btn btn-success btn-sm"
+          @click="addCustomers">Add
+          </button>
+          <button type="reset"
+          class="btn btn-dark btn-sm"
+          @click="testSwal">reset
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      name: '',
+      numberPhone: '',
+      newId: ''
+    }
+  },
+  methods: {
+    addCustomers: function() {
+      var getCustomers = this.$store.getters.getCountCostumers
+      this.newId = getCustomers + 1
+      this.$store.dispatch('addCustomers', this)
+      this.name = ''
+      this.numberPhone = ''
+      this.newId = ''
+    },
+    testSwal: function() {
+      // this.$swal('Testing', 'Sweet-Alert-Berhasil', 'OK')
+      this.$swal('Any fool can use a computer')
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
