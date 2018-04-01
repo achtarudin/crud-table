fc193788368a0cacb0d7c0b962d832daf02be16a
achtarudin <cabe2cabean214@gmail.com>
2018-04-01 16:02:58 +0700

    add component Table.vue

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,41 +1,44 @@
 <template>
-<div class="card card-body border border-primary">
-  <table class="table table-striped table-hover">
-    <thead class="">
-      <tr class="text-center">
-        <th scope="col">ID</th>
-        <th scope="col">Name</th>
-        <th scope="col">Number Phone</th>
-        <th scope="col">Edit</th>
-        <th scope="col">Delete</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="customer in customers"
-      :key="customer.id"
-      class="text-center">
-        <th>{{customer.id}}</th>
-        <th>{{customer.name}}</th>
-        <th>{{customer.numberPhone}}</th>
-        <th><button class="btn btn-outline-info btn-sm">Edit</button></th>
-        <th><button class="btn btn-outline-danger btn-sm">Delete</button></th>
-      </tr>
-    </tbody>
-  </table>
-</div>
+  <div>
+    <table class="table table-hover">
+      <thead>
+        <tr class="text-center">
+          <th scope="col">ID</th>
+          <th scope="col">Name</th>
+          <th scope="col">Number Phone</th>
+          <th scope="col"></th>
+          <th scope="col"></th>
+
+        </tr>
+      </thead>
+      <tbody>
+        <tr scope="row" v-for="customer in custumers" :key="customer.id">
+          <td class="text-center">{{customer.id}}</td>
+          <td class="text-center">{{customer.name}}</td>
+          <td class="text-center">{{customer.numberPhone}}</td>
+          <td class="text-center">
+            <button type="button" class="btn btn-outline-primary btn-sm">edit</button>
+          </td>
+          <td class="text-center">
+            <button type="button" class="btn btn-outline-danger btn-sm">delete</button>
+          </td>
+
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
   computed: {
-    customers: function () {
-      let customers = this.$store.getters.getCustomers
-      return customers
+    custumers: function() {
+      var getCustomers = this.$store.getters.getCustomers
+      return getCustomers
     }
   }
 }
 </script>
 
 <style>
-
 </style>
