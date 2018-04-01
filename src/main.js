a3d601ca08646fc65d2b129d2aacb0d92d015dac
achtarudin <cabe2cabean214@gmail.com>
2018-04-01 16:03:51 +0700

    import library sweetAlert2 in main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import store from './assets/store/store.js'
+import VueSweetalert2 from 'vue-sweetalert2'
 Vue.config.productionTip = false
+Vue.use(VueSweetalert2)
 
 /* eslint-disable no-new */
 new Vue({
