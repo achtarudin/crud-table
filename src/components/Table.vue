65fa1151353fae88f8024183995068ec0e2be3ba
achtarudin <cabe2cabean214@gmail.com>
2018-04-02 17:10:50 +0700

    setup delet button for delete user

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -8,24 +8,28 @@
           <th scope="col">Number Phone</th>
           <th scope="col"></th>
           <th scope="col"></th>
-
         </tr>
       </thead>
       <tbody>
         <tr scope="row" v-for="customer in custumers" :key="customer.id">
           <td class="text-center">{{customer.id}}</td>
-          <td class="text-center">{{customer.name}}</td>
+          <td class="text-center">{{capitalize(customer.name)}}</td>
           <td class="text-center">{{customer.numberPhone}}</td>
           <td class="text-center">
             <button type="button"
             class="btn btn-outline-primary btn-sm"
-            @click="editCustomer" :value="customer.id">
+            @click="editCustomer"
+            :value="customer.id">
             edit</button>
           </td>
           <td class="text-center">
-            <button type="button" class="btn btn-outline-danger btn-sm">delete</button>
+            <button type="button"
+            class="btn btn-outline-danger btn-sm"
+            @click="deleteCustomer"
+            :value="customer.id">
+            delete
+            </button>
           </td>
-
         </tr>
       </tbody>
     </table>
@@ -44,10 +48,21 @@ export default {
     editCustomer: function(e) {
       var id = e.target.value
       this.$store.dispatch('editCustomer', id)
+    },
+    // method delete costumer mutation
+    deleteCustomer: function(e) {
+      var id = e.target.value
+      this.$store.dispatch('deleteCustomer', id)
+    },
+    // methods for capital word
+    capitalize(name) {
+      var capitalize = name.replace(/\b\w/g, l => l.toUpperCase())
+      return capitalize
     }
   }
 }
 </script>
 
 <style>
+
 </style>
