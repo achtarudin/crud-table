ac91d94f94abf1a2c5b03f5816177cf85be7f68c
achtarudin <cabe2cabean214@gmail.com>
2018-04-03 15:11:03 +0700

    move import store to page.vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import store from './assets/store/store.js'
+// import store from './assets/store/store.js'
 import VueSweetalert2 from 'vue-sweetalert2'
 Vue.config.productionTip = false
 Vue.use(VueSweetalert2)
@@ -11,6 +9,6 @@ Vue.use(VueSweetalert2)
 new Vue({
   el: '#app',
   components: { App },
-  template: '<App/>',
-  store
+  template: '<App/>'
+  // store
 })
