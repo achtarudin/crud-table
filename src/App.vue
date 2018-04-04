63cb2f6a7efc0c52be3da954c03b32f863dc0846
achtarudin <cabe2cabean214@gmail.com>
2018-04-04 10:37:40 +0700

    remake App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,39 @@
     <br>
     <div class="row justify-content-center">
       <div class="col-md-8">
-        <pages></pages>
+        <add-edit @addCustomer="ADD_CUSTOMER">
+        </add-edit>
+        <br>
+        <tables :customer="COSTUMERS"></tables>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Pages from './components/Pages.vue'
+import AddEdit from './components/AddEdit'
+import Tables from './components/Table'
+import { COSTUMERS } from './assets/store/customers'
 export default {
+  data() {
+    return {
+      COSTUMERS
+    }
+  },
   components: {
-    Pages
+    AddEdit,
+    Tables
+  },
+  methods: {
+    ADD_CUSTOMER(payload) {
+      console.log(payload.name)
+      console.log(payload.numberPhone)
+      console.log(this.COSTUMERS.length)
+    }
   }
 }
 </script>
 
 <style>
+
 </style>
