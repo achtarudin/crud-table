6b6683829786009b2a74995c8d7ccc7694a34230
achtarudin <cabe2cabean214@gmail.com>
2018-04-06 23:36:50 +0700

    add plugin sweet alert 2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App'
-// import store from './assets/store/store.js'
 import VueSweetalert2 from 'vue-sweetalert2'
 Vue.config.productionTip = false
 Vue.use(VueSweetalert2)
